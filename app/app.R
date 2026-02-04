5b78c70d74f2ff39b18b88c36a09a9e25a2495ab
Anna Benýšková <anna.benyskova@gmail.com>
2026-02-04 12:03:27 +0100

    update app.R

--- app/app.R
+++ app/app.R
@@ -87,21 +87,22 @@ ui <- page_sidebar(
     # Link to OMIM database
     tags$a(
       href = "https://www.omim.org", target = "_blank",
-      style = "font-weight: bold; font-size: 16px; display: block; text-align: center;", # nolint
+      style = "font-weight: bold; font-size: 16px; display: block; margin-top: 10px;", # nolint
       icon("database"), "OMIM databáze"
     ),
     br(), br(), br(), br(), br(), br(), br(),
-    #helpText(HTML("V případě dotazů pište na tento <a href='mailto:Benyskova.Anna@fnbrno.cz'>e-mail</a>")),
+    #helpText(HTML("V případě dotazů pište na tento <a href='mailto:Benyskova.Anna@fnbrno.cz'>e-mail</a>")),   
   ),
-
+  
   # top card
+  uiOutput("aktualizace"),
   uiOutput("panel_karta"),
   # bottom tab
   card(
     navset_card_tab(
-      nav_panel("Coverage", DT::dataTableOutput("coverage_table")),
-      nav_panel("CNV Muži", DT::dataTableOutput("cnv_m")),
-      nav_panel("CNV Ženy", DT::dataTableOutput("cnv_z"))
+      nav_panel("Table", DT::dataTableOutput("coverage_table")),
+      nav_panel("CNV M", DT::dataTableOutput("cnv_m")),
+      nav_panel("CNV Z", DT::dataTableOutput("cnv_z"))
     )
   )
 )
@@ -110,33 +111,45 @@ ui <- page_sidebar(
 
 # --- Server logic ---
 server <- function(input, output, session) {
-
-  # actualization
-  #output$aktualizace <- renderUI({
-  #  if (is.null(input$file)) {
-  #    card(
-  #      tags$div(
-  #        class = "text-left",
-  #        style = "margin-bottom: 10px;",
-  #        tags$p("Aktualizace", style = "font-weight: bold; color: #007BC2; font-size: 16px;"), # nolint
-  #        tags$p("Do CNV_M a CNV_Z přidán sloupce Row_id pro číslování řádků odpovídající původnímu pořadí.", style = "font-size: 14px;"), # nolint
-  #        tags$p("Automatické přejmenování souborů s příponou '(paired) Target Region Coverage'.", style = "font-size: 14px;"), # nolint
-  #      ),
-  #    )
-  #  } else {
-  #    card(
-  #      uiOutput("gender_input"),
-  #      uiOutput("action_button")
-  #    )
-  #  }
+  output$aktualizace <- renderUI({
+    if (is.null(input$file)) {
+      card(
+        tags$div(
+          class = "text-left",
+          style = "margin-bottom: 10px;",
+          tags$p("Aktualizace", style = "font-weight: bold; color: #007BC2; font-size: 20px;"), # nolint
+          tags$p("Interaktivní velikost okna a přidané posuvníky"),
+          tags$p("Do názvu exportu přidané datum"),
+          tags$p("Zdrojové soubory .txt musí obsahovat tyto sloupce: 1. Chromosome, 2. Region, 3. Name a 15. Mean_coverage"),
+          tags$p("Do CNV_M a CNV_Z přidán sloupce Row_id pro číslování řádků odpovídající původnímu pořadí."), # nolint
+          tags$p("Automatické přejmenování souborů s příponou '(paired) Target Region Coverage'"), # nolint
+          tags$br(),
+          tags$p("Kdyby něco nejelo, nebojte se mi napsat"),
+          tags$a("Benyskova.Anna@fnbrno.cz", 
+                 href = "mailto:Benyskova.Anna@fnbrno.cz",
+                 style = "font-size: 14px;")
+        ),
+      )
+    } else {
+      NULL
+    }
+  })
+  
+  #output$text <- renderText({
+  #  if (is.null(input$file)) return("Kód várky: ")
+  #  #base_names <- sub("_cov\\.txt|| \\(paired\\) Target Region Coverage\\.txt$", "", input$file$name) # nolint
+  #  #codes <- substr(base_names, nchar(base_names) - 1, nchar(base_names)) # nolint
+  #  codes <- substr(input$file$name, 8, 9)
+  #  paste("Kód várky: ", unique(codes), collapse = ", ")
   #})
-
+  
   # Name of samples without suffixes
   sample_id <- reactive({
     req(input$file)
+    #gsub("_cov\\.txt|| \\(paired\\) Target Region Coverage\\.txt$", "", input$file$name)
     gsub("(_cov\\.txt|[ _]*\\(paired\\)[ _]*Targ(et|ed)?[ _]*Region[ _]*Coverage\\.txt)$", "", input$file$name) # nolint
   })
-
+  
   # Take the last two letters from name of samples
   output$text <- renderText({
     req(sample_id())
@@ -144,7 +157,7 @@ server <- function(input, output, session) {
     codes <- substr(sample_id(), nchar(sample_id()) - 1, nchar(sample_id())) # nolint
     paste("Kód várky: ", unique(codes), collapse = ", ")
   })
-
+  
   output$panel_karta <- renderUI({
     req(input$file)
     card(
@@ -158,54 +171,63 @@ server <- function(input, output, session) {
       uiOutput("action_button")
     )
   })
-
+  
   # Button for choosing gender
   output$gender_input <- renderUI({
     ids <- sample_id()
     lapply(ids, function(id) {
       div(class = "gender-row",
-        div(class = "gender-label", strong(id)),
-        div(class = "gender-select",
-          selectInput(
-            inputId = paste0("pohlavi", id),
-            label = NULL,
-            choices = c("Muž" = "M", "Žena" = "Z"),
-            width = "80px",
-            selectize = TRUE
+          div(class = "gender-label", strong(id)),
+          div(class = "gender-select",
+              selectInput(
+                inputId = paste0("pohlavi", id),
+                label = NULL,
+                choices = c("Muž" = "M", "Žena" = "Z"),
+                width = "80px",
+                selectize = TRUE
+              )
           )
-        )
       )
     })
   })
-
+  
   final_data <- reactiveVal()
   pohlavi_data <- reactiveVal()
   cnv_m_data <- reactiveVal()
   cnv_z_data <- reactiveVal()
   submit_status <- reactiveVal("ready")
-
+  
   # Button to process input and run analysis
   output$action_button <- renderUI({
     req(input$file)
-    actionButton(
-      "submit",
-      label = if (submit_status() == "processing") "Zpracovávám..." else "Zpracovat", # nolint
-      icon = if (submit_status() != "processing") icon("check") else NULL,
-      class = if (submit_status() == "processing") "btn btn-primary" else "btn btn-success", # nolint
-      style = "margin-top: 5px; width: 200px; font-size: 20px; padding: 10px;",
-      disabled = submit_status() == "processing"
-    )
+    if (submit_status() == "processing") {
+      actionButton(
+        "submit",
+        label = "Zpracovávám...",
+        icon = icon("spinner", class = "fa-spin"),
+        class = "btn btn-primary",
+        style = "margin-top: 5px; width: 200px; font-size: 20px; padding: 10px;"
+      )
+    } else {
+      actionButton(
+        "submit",
+        label = "Zpracovat",
+        icon = icon("check"),
+        class = "btn btn-success",
+        style = "margin-top: 5px; width: 200px; font-size: 20px; padding: 10px;"
+      )
+    }
   })
-
+  
   # --- Makeing Tables Coverages and CNV ---
   observeEvent(input$submit, {
     req(input$file)
     submit_status("processing")
-
+    
     withProgress(message = "Zpracování CNV...", value = 0, {
       submit_status("processing")
       incProgress(0.1, detail = "Načítání souborů...")
-
+      
       file_list <- input$file$datapath
       filenames <- input$file$name
       ids <- sample_id()
@@ -213,44 +235,53 @@ server <- function(input, output, session) {
       pohlavi_df <- data.frame(ID = ids, Gender = pohlavi)
       pohlavi_data(pohlavi_df)
       if (!dir.exists("../data_output")) dir.create("../data_output")
-      #write.csv(pohlavi_df, "../data_output/pohlavi.csv", row.names = FALSE) # nolint
-
-      #showNotification("Soubory coverage a CNV se generují.", type = "message") # nolint
-
+      #write.csv(pohlavi_df, "../data_output/pohlavi.csv", row.names = FALSE)
+      
       incProgress(0.3, detail = "Generování coverage dat...")
-
+      
+      #showNotification("Soubory coverage a CNV se generují.", type = "message")
+      
       selected_cols_list <- lapply(seq_along(file_list), function(i) {
         tryCatch({
           df <- read.delim(file_list[i], check.names = FALSE)
           if (nrow(df) < 1 || ncol(df) < 15) stop()
           selected <- df[, 15, drop = FALSE]
-          base_name <- tools::file_path_sans_ext(gsub("(_cov\\.txt|[ _]*\\(paired\\)[ _]*Targ(et|ed)?[ _]*Region[ _]*Coverage\\.txt)$", "", filenames[i])) # nolint
+          #base_name <- tools::file_path_sans_ext(gsub("_cov\\.txt|| \\(paired\\) Target Region Coverage\\.txt$", "", filenames[i])) # nolint
+          base_name <- tools::file_path_sans_ext(
+            gsub("(_cov\\.txt|[ _]*\\(paired\\)[ _]*Targ(et|ed)?[ _]*Region[ _]*Coverage\\.txt)$", "", filenames[i])
+          )
           gender <- input[[paste0("pohlavi", ids[i])]]
           colnames(selected) <- paste0(gender, "_", base_name)
-          return(selected)
           print(gender)
+          return(selected)
         }, error = function(e) {
           showNotification(paste("Chyba u souboru:", filenames[i]), type = "error") # nolint
           return(NULL)
         })
       })
-
+      
       result <- do.call(cbind, selected_cols_list)
       prvni_trisloupce <- read.delim(file_list[1], check.names = FALSE)[, 1:3]
       combined <- cbind(prvni_trisloupce, result)
       colnames(combined) <- trimws(gsub("Mean coverage", "", colnames(combined), fixed = TRUE)) # nolint
       final_data(combined)
-
+      
       incProgress(0.6, detail = "Normalizace CNV M...")
-
+      
+      #tryCatch({
+      #  write.table(combined, "../data_output/coverage.csv", col.names = TRUE, row.names = FALSE, sep = ";") # nolint
+      #}, error = function(e) {
+      #  showNotification("Chyba při ukládání coverage.csv", type = "error")
+      #})
+      
       # CNV logic
       coverage <- final_data()
       pohlavi <- pohlavi_data()
-      row_id <- seq.int(nrow(coverage))
+      #row_id <- seq.int(nrow(coverage))
       m <- colnames(coverage)[grepl("^M_", colnames(coverage))]
       z <- colnames(coverage)[grepl("^Z_", colnames(coverage))]
       omimgeny <- load_omim_file()
-
+      
       # Men
       if (length(m) > 0) {
 
@@ -258,7 +289,7 @@ server <- function(input, output, session) {
         normalized_m <- normalize_coverage(coverage[, m, drop = FALSE])
         #cat("---normalized_m--- \n")
         #print(head(normalized_m, 5))
-        cat("normalized_m has", nrow(normalized_m), "rows and", ncol(normalized_m), "columns\n") # nolint
+        #cat("normalized_m has", nrow(normalized_m), "rows and", ncol(normalized_m), "columns\n") # nolint
 
         # Add Row_id to coverage data
         coverage$Row_id <- seq.int(nrow(coverage))
@@ -266,7 +297,7 @@ server <- function(input, output, session) {
           coverage[, c("Chromosome", "Region", "Name", "Row_id")], # check the name of the columns # nolint
           normalized_m
         )
-        #coverage_m_final <- cbind(coverage[, c(1:3)], Row_id = seq.int(nrow(coverage)), normalized_m) # nolint
+	      #coverage_m_final <- cbind(coverage[, c(1:3)], Row_id = row_id, normalized_m) # nolint
         #cat("---coverage_m_final--- \n")
         #print(head(coverage_m_final, 5))
 
@@ -282,17 +313,14 @@ server <- function(input, output, session) {
 
         # Filter rows where any m_value is greater than 0
         greater_m <- coverage_m_final[rowSums(m_values, na.rm = TRUE) > 0, ]
-        #cat("---greater_m--- \n")
-        #print(head(greater_m, 5))
-
-        # Annotate greater_m with OMIM data
+        
         greater_m <- annotate_with_omim(greater_m, omimgeny)
         #write.table(greater_m, "../data_output/CNV_M.csv", sep = ";", row.names = FALSE) # nolint
         cnv_m_data(greater_m)
       }
-
+      
       incProgress(0.8, detail = "Normalizace CNV Z...")
-
+      
       # Women
       if (length(z) > 0) {
 
@@ -331,7 +359,7 @@ server <- function(input, output, session) {
   #output$coverage_table <- renderTable({ final_data() })
   #output$cnv_m <- renderTable({ cnv_m_data() })
   #output$cnv_z <- renderTable({ cnv_z_data() })
-
+  
   # Render coverage table
   output$coverage_table <- DT::renderDataTable({
     req(final_data())
@@ -345,7 +373,7 @@ server <- function(input, output, session) {
       )
     )
   })
-
+  
   # Render CNV M and CNV Z tables
   output$cnv_m <- DT::renderDataTable({
     req(cnv_m_data())
@@ -374,19 +402,28 @@ server <- function(input, output, session) {
 
   # --- Download handlers for coverage and CNV data
   output$downloadCoverage <- downloadHandler(
-    filename = function() { "coverage.csv" },
+    #filename = function() { "coverage.csv" },
+    filename = function() { 
+      paste0("coverage_", format(Sys.time(), "%Y%m%d"), ".csv") 
+    },
     content = function(file) {
       write.csv2(final_data(), file, row.names = FALSE, quote = FALSE, fileEncoding = "UTF-8") # nolint
     }
   )
   output$downloadCNVM <- downloadHandler(
-    filename = function() { "CNV_M.csv" },
+    #filename = function() { "CNV_M.csv" },
+    filename = function() { 
+      paste0("CNV_M_", format(Sys.time(), "%Y%m%d"), ".csv") 
+    },
     content = function(file) {
       write.csv2(cnv_m_data(), file, row.names = FALSE, quote = FALSE, fileEncoding = "UTF-8") # nolint
     }
   )
   output$downloadCNVZ <- downloadHandler(
-    filename = function() { "CNV_Z.csv" },
+    #filename = function() { "CNV_Z.csv" },
+    filename = function() { 
+      paste0("CNV_Z_", format(Sys.time(), "%Y%m%d"), ".csv") 
+    },
     content = function(file) {
       write.csv2(cnv_z_data(), file, row.names = FALSE, quote = FALSE, fileEncoding = "UTF-8") # nolint
     }
